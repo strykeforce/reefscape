98a7e84526a4b46cbcc02df05b3860d37318cdff
mwitcpalek <mwitcpalek@gmail.com>
2025-01-08 18:12:30 -0500

    fix build errors

--- src/main/java/frc/robot/subsystems/algae/AlgaeSubsystem.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeSubsystem.java
@@ -1,15 +1,3 @@
 package frc.robot.subsystems.algae;
-package frc.robot.standards;
-import edu.wpi.first.units.measure.AngularVelocity;
 
-
-public class AlgaeSubsystem {
-
-
-
-  public void setSpeed(AngularVelocity speed){};
-
-  public AngularVelocity getSpeed(){};
-
-  public boolean atSpeed(){};
-}
+public class AlgaeSubsystem {}
