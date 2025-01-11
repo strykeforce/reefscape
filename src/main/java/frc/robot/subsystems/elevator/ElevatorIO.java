8669d8222fe0bb15d5e023184007a9c377c12f84
mwitcpalek <mwitcpalek@gmail.com>
2025-01-10 20:30:29 -0500

    fix build errors

--- src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIO.java
@@ -1,15 +1,3 @@
 package frc.robot.subsystems.elevator;
 
-import org.littletonrobotics.junction.AutoLog;
-import org.strykeforce.telemetry.TelemetryService;
-
-public interface ElevatorIO {
-    public double position = 0.0;
-    public double velocityLeft = 0.0;
-    public double velocityRight = 0.0;
-
-    public default void updateInputs(ExiterIOInputs inputs) {}
-
-    public default void registerWith(TelemetryService telemetryService) {}
-    
-}
+public interface ElevatorIO {}
--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -1,11 +1,3 @@
 package frc.robot.subsystems.elevator;
 
-public class ElevatorSubsystem {
-    // Private Variables
-    private final ElevatorIO io;
-
-    // Constructor
-    public void ExiterSubsystem(ElevatorIO io) {
-        this.io = io;
-  }
-}
+public class ElevatorSubsystem {}
