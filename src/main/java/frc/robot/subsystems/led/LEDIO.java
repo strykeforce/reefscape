f931e745aab47b995450b269b325181e060fc488
Isaac Hoekstra <122327870+PotatoBoyH4@users.noreply.github.com>
2025-01-16 17:04:09 -0500

    started the LEDsubsystem

--- src/main/java/frc/robot/subsystems/led/LEDIO.java
+++ src/main/java/frc/robot/subsystems/led/LEDIO.java
@@ -1,3 +1,59 @@
 package frc.robot.subsystems.led;
 
-public class LEDIO {}
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.util.Color;
+
+public class LEDIO {
+  public AddressableLED ledBase;
+  public AddressableLEDBuffer led;
+
+  public LEDIO(int port, int length) {
+    ledBase = new AddressableLED(port);
+    ledBase.setLength(length);
+    ledBase.start();
+    led = new AddressableLEDBuffer(length);
+    ledBase.setData(led);
+  }
+
+  public void setPort(int port) {
+    ledBase.close();
+    ledBase = new AddressableLED(port);
+    ledBase.setLength(led.getLength());
+    ledBase.start();
+    ledBase.setData(led);
+  }
+
+  public void setLength(int length) {
+    ledBase.setLength(length);
+    led = new AddressableLEDBuffer(length);
+    ledBase.setData(led);
+  }
+
+  public void updateLEDs() {
+    ledBase.setData(led);
+  }
+
+  public void setLED(int index, Color color) {
+    led.setLED(index, color);
+  }
+
+  public void setLED(int index, int r, int g, int b) {
+    led.setRGB(index, r, g, b);
+  }
+
+  public Color getLED(int index) {
+    return led.getLED(index);
+  }
+
+  public void setOff() {
+    for (int i = 0; i < led.getLength(); i++) {
+      setLED(i, Color.kBlack);
+    }
+  }
+
+  public void setPattern(LEDPattern pattern) {
+    pattern.applyTo(led);
+  }
+}
--- src/main/java/frc/robot/subsystems/led/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/led/LEDSubsystem.java
@@ -1,3 +1,26 @@
 package frc.robot.subsystems.led;
 
-public class LEDSubsystem {}
+import java.util.Set;
+
+import org.strykeforce.telemetry.measurable.MeasurableSubsystem;
+import org.strykeforce.telemetry.measurable.Measure;
+
+public class LEDSubsystem extends MeasurableSubsystem{
+    public LEDStates currState = LEDStates.OFF; 
+    
+    public LEDSubsystem() {
+        
+    }
+
+    @Override
+    public Set<Measure> getMeasures() {
+        return Set.of(new Measure("state", "the current state of the LEDSubsystem", () -> currState.ordinal()));
+    }
+
+    public enum LEDStates {
+        OFF,
+        SOLID,
+        FLAMES,
+        RAINBOW
+    }
+}
