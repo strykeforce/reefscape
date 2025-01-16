813a15f7ebc588406a40fe3f55cdf84c45aae705
EpicPigGuy <epicpigguysminecraft@gmail.com>
2025-01-16 18:16:57 -0500

    Update algaeIO.java

--- src/main/java/frc/robot/subsystems/algae/algaeIO.java
+++ src/main/java/frc/robot/subsystems/algae/algaeIO.java
@@ -4,10 +4,13 @@ import edu.wpi.first.units.measure.AngularVelocity;
 import org.littletonrobotics.junction.AutoLog;
 import org.strykeforce.telemetry.TelemetryService;
 
+import com.ctre.phoenix6.signals.ReverseLimitValue;
+
 public interface algaeIO {
   @AutoLog
   public static class IOInputs {
     public AngularVelocity velocity;
+    public ReverseLimitValue reverseLimitSwitch;
   }
 
   public default void updateInputs(IOInputs inputs) {}
