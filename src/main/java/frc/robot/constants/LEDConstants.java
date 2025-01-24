a78dcc7c7577c59f8c8b2955a51114d29bc87cc3
Isaac Hoekstra <122327870+PotatoBoyH4@users.noreply.github.com>
2025-01-23 21:01:26 -0500

    spotlessApply

--- src/main/java/frc/robot/constants/LEDConstants.java
+++ src/main/java/frc/robot/constants/LEDConstants.java
@@ -3,24 +3,24 @@ package frc.robot.constants;
 import edu.wpi.first.wpilibj.util.Color;
 
 public class LEDConstants {
-    public static final int stripLength = 42;
+  public static final int stripLength = 42;
 
-    public static final Color kAlmostBlack = new Color(0, 0, 1);
-    // LED Colors
-    public static final Color kHasAlgea = Color.kAquamarine;
-    public static final Color kNotHasAlgea = kAlmostBlack;
-    public static final Color kCoralNotInRobot = Color.kOrange;
-    public static final Color kCoralInFunnel = Color.kHotPink;
-    public static final Color kCoralInRobot = Color.kWhite;
-    public static final Color kCurrentLimiting = Color.kRed;
-    public static final Color kAutoPlacing = Color.kPurple;
-    public static final Color kManual = kAlmostBlack;
-    public static final Color kRight = Color.kSaddleBrown;
-    public static final Color kLeft = Color.kGreenYellow;
-    public static final Color kL1 = Color.kBlue;
-    public static final Color kL2 = Color.kGreen;
-    public static final Color kL3 = Color.kYellow;
-    public static final Color kL4 = Color.kRed;
-    public static final Color kGettingAlgea = Color.kAquamarine;
-    public static final Color kNotGettingAlgea = kAlmostBlack;
+  public static final Color kAlmostBlack = new Color(0, 0, 1);
+  // LED Colors
+  public static final Color kHasAlgea = Color.kAquamarine;
+  public static final Color kNotHasAlgea = kAlmostBlack;
+  public static final Color kCoralNotInRobot = Color.kOrange;
+  public static final Color kCoralInFunnel = Color.kHotPink;
+  public static final Color kCoralInRobot = Color.kWhite;
+  public static final Color kCurrentLimiting = Color.kRed;
+  public static final Color kAutoPlacing = Color.kPurple;
+  public static final Color kManual = kAlmostBlack;
+  public static final Color kRight = Color.kSaddleBrown;
+  public static final Color kLeft = Color.kGreenYellow;
+  public static final Color kL1 = Color.kBlue;
+  public static final Color kL2 = Color.kGreen;
+  public static final Color kL3 = Color.kYellow;
+  public static final Color kL4 = Color.kRed;
+  public static final Color kGettingAlgea = Color.kAquamarine;
+  public static final Color kNotGettingAlgea = kAlmostBlack;
 }
--- src/main/java/frc/robot/subsystems/led/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/led/LEDSubsystem.java
@@ -2,84 +2,93 @@ package frc.robot.subsystems.led;
 
 import static edu.wpi.first.units.Units.Seconds;
 
-import java.util.Map;
-import java.util.Set;
-
-import org.strykeforce.telemetry.measurable.MeasurableSubsystem;
-import org.strykeforce.telemetry.measurable.Measure;
-
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
 import frc.robot.constants.LEDConstants;
+import java.util.Map;
+import java.util.Set;
+import org.strykeforce.telemetry.measurable.MeasurableSubsystem;
+import org.strykeforce.telemetry.measurable.Measure;
 
-public class LEDSubsystem extends MeasurableSubsystem{
-    private LEDIO io;
-    private LEDPattern base = LEDPattern.solid(Color.kBlack);
-    private LEDPattern coral = LEDPattern.solid(LEDConstants.kCoralInRobot);
-    private LEDPattern algea = LEDPattern.steps(Map.of(0, LEDConstants.kHasAlgea, LEDConstants.stripLength/3, Color.kBlack));
+public class LEDSubsystem extends MeasurableSubsystem {
+  private LEDIO io;
+  private LEDPattern base = LEDPattern.solid(Color.kBlack);
+  private LEDPattern coral = LEDPattern.solid(LEDConstants.kCoralInRobot);
+  private LEDPattern algea =
+      LEDPattern.steps(
+          Map.of(0, LEDConstants.kHasAlgea, LEDConstants.stripLength / 3, Color.kBlack));
 
-    private LEDPattern currentLimiting = LEDPattern.solid(LEDConstants.kCurrentLimiting).blink(Seconds.of(1), Seconds.of(2));
-    private LEDPattern autoplace = LEDPattern.steps(Map.of(LEDConstants.stripLength/3*2, LEDConstants.kAutoPlacing)).blink(Seconds.of(0.25));
-    public LEDStates currState = LEDStates.OFF;
-    public CoralStates coralState = CoralStates.NO_PIECE; 
-    public boolean autoPlacing = false;
-    public boolean hasAlgae = false;
-    public boolean isLimiting = false;
-    
-    public LEDSubsystem(LEDIO io) {
-        this.io = io;
+  private LEDPattern currentLimiting =
+      LEDPattern.solid(LEDConstants.kCurrentLimiting).blink(Seconds.of(1), Seconds.of(2));
+  private LEDPattern autoplace =
+      LEDPattern.steps(Map.of(LEDConstants.stripLength / 3 * 2, LEDConstants.kAutoPlacing))
+          .blink(Seconds.of(0.25));
+  public LEDStates currState = LEDStates.OFF;
+  public CoralStates coralState = CoralStates.NO_PIECE;
+  public boolean autoPlacing = false;
+  public boolean hasAlgae = false;
+  public boolean isLimiting = false;
+
+  public LEDSubsystem(LEDIO io) {
+    this.io = io;
+  }
+
+  private void buildBase() {
+    switch (coralState) {
+      case IN_FUNNEL:
+        coral = LEDPattern.solid(LEDConstants.kCoralInFunnel);
+        break;
+      case IN_ROBOT:
+        coral = LEDPattern.solid(LEDConstants.kCoralInRobot);
+        break;
+      case NO_PIECE:
+        coral = LEDPattern.solid(LEDConstants.kCoralNotInRobot);
+        break;
     }
+    algea =
+        LEDPattern.steps(
+            Map.of(0, LEDConstants.kHasAlgea, LEDConstants.stripLength / 3, Color.kBlack));
+  }
 
-    private void buildBase() {
-        switch (coralState) {
-            case IN_FUNNEL:
-                coral = LEDPattern.solid(LEDConstants.kCoralInFunnel);
-                break;
-            case IN_ROBOT:
-                coral = LEDPattern.solid(LEDConstants.kCoralInRobot);
-                break;
-            case NO_PIECE:
-                coral = LEDPattern.solid(LEDConstants.kCoralNotInRobot);
-                break;
+  public void periodic() {
+    switch (currState) {
+      case OFF:
+        break;
+      case NORMAL:
+        if (autoPlacing) {
+          base = autoplace.overlayOn(base);
         }
-        algea = LEDPattern.steps(Map.of(0, LEDConstants.kHasAlgea, LEDConstants.stripLength/3, Color.kBlack));
-        
-    }
-
-    public void periodic() {
-        switch (currState) {
-            case OFF:
-                break;
-            case NORMAL:
-                if (autoPlacing) {
-                    base = autoplace.overlayOn(base);
-                }
-                if (isLimiting) {
-                    base = currentLimiting.overlayOn(base);
-                }
-                io.setStrip(base);
-                break;
-            case CLIMB:
-                break;
+        if (isLimiting) {
+          base = currentLimiting.overlayOn(base);
         }
-        io.updateLEDs();
+        io.setStrip(base);
+        break;
+      case CLIMB:
+        break;
     }
+    io.updateLEDs();
+  }
 
-    @Override
-    public Set<Measure> getMeasures() {
-        return Set.of(new Measure("State", "the current Overall state of the LEDSubsystem", () -> currState.ordinal()),
-            new Measure("CoralState", "the current Coral state of the LEDSubsystem", () -> coralState.ordinal()));
-    }
+  @Override
+  public Set<Measure> getMeasures() {
+    return Set.of(
+        new Measure(
+            "State", "the current Overall state of the LEDSubsystem", () -> currState.ordinal()),
+        new Measure(
+            "CoralState",
+            "the current Coral state of the LEDSubsystem",
+            () -> coralState.ordinal()));
+  }
 
-    public enum LEDStates {
-        OFF,
-        NORMAL,
-        CLIMB
-    }
+  public enum LEDStates {
+    OFF,
+    NORMAL,
+    CLIMB
+  }
 
-    public enum CoralStates {
-        IN_FUNNEL,
-        IN_ROBOT,
-        NO_PIECE,
-    }
+  public enum CoralStates {
+    IN_FUNNEL,
+    IN_ROBOT,
+    NO_PIECE,
+  }
 }
