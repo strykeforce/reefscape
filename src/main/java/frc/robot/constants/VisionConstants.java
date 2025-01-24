2254e0e4a00114d21911288b5dde6882b010777e
mwitcpalek <mwitcpalek@gmail.com>
2025-01-23 20:47:09 -0500

    fix build issues

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -24,7 +24,6 @@ public final class VisionConstants {
   //   public static final double kThetaStdDevRejected = Units.degreesToRadians(360);
   //   public static final double kThetaStdThres = 0.2;
 
-
   // Velocity Filter
   public static final double kLinearCoeffOnVelFilter = 0.1;
   public static final double kOffsetOnVelFilter = 0.10;
