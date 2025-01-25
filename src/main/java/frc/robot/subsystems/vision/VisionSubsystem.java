f8ac89c5a1e6607b885701030021f91ae7a5368a
David Shen <dshen1248@gmail.com>
2025-01-25 15:18:45 -0500

    fix math

--- src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
@@ -3,12 +3,11 @@ package frc.robot.subsystems.vision;
 import WallEye.WallEyeTagResult;
 
 public class VisionSubsystem {
-    public VisionSubsystem() {
-    }
+  public VisionSubsystem() {}
 
-    // FIXME: see walleye-testing branch from crescendo for correct implementation but
-    // FIXME: USE WalleyeTagResult INSTEAD OF WalleyeResult!
-    public WallEyeTagResult getLastResult(int index) {
-        return null; 
-    } 
+  // FIXME: see walleye-testing branch from crescendo for correct implementation but
+  // FIXME: USE WalleyeTagResult INSTEAD OF WalleyeResult!
+  public WallEyeTagResult getLastResult(int index) {
+    return null;
+  }
 }
