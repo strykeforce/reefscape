4fc6b57527ce50290d4bffa91fed50681d7dae8f
Huck-Richardson <huck.richardson@gmail.com>
2025-01-27 20:38:20 -0500

    Added getter methods for tag info

--- src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.vision;
 
+import static edu.wpi.first.units.Units.*;
+
 import WallEye.*;
 import WallEye.UdpSubscriber;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
@@ -10,20 +12,15 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.units.measure.Distance;
 import frc.robot.constants.VisionConstants;
 import frc.robot.subsystems.drive.DriveSubsystem;
 import frc.robot.subsystems.drive.Swerve;
-
-import static edu.wpi.first.units.Units.Meters;
-
 import java.io.IOException;
 import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.strykeforce.telemetry.measurable.MeasurableSubsystem;
 import org.strykeforce.telemetry.measurable.Measure;
-import java.lang.Math;
 
 public class VisionSubsystem extends MeasurableSubsystem {
 
@@ -88,54 +85,79 @@ public class VisionSubsystem extends MeasurableSubsystem {
     udpSubscriber[1] = new UdpSubscriber(1, cams[2], cams[3]);
     udpSubscriber[2] = new UdpSubscriber(2, cams[4]);
   }
-
+  // Setter Methods
   public void setVisionUpdating(boolean updating) {
     this.updating = updating;
   }
 
-  public boolean isVisionUpdating() {
-    return updating;
-  }
-
   public void setMinTags(int minTags) {
     this.minTags = minTags;
+  }
+  // Getter Methods
+  public boolean isVisionUpdating() {
+    return updating;
   }
 
   public boolean cameraConnected(int index) {
     return cams[index].isCameraConnected();
   }
 
-  //Filters
+  private double minTagDistance(WallEyePoseResult result) {
+
+    Translation2d camLoc = result.getCameraPose().getTranslation().toTranslation2d();
+    int[] ids = result.getTagIDs();
+    double minDistance = 2767;
+
+    for (int id : ids) {
+      Translation2d tagLoc = field.getTagPose(id).get().getTranslation().toTranslation2d();
+      double dist = camLoc.getDistance(camLoc);
+
+      if (dist < minDistance) {
+        minDistance = dist;
+      }
+    }
+    return minDistance;
+  }
+
+  private double averageTagDistance(WallEyePoseResult result) {
+
+    Translation2d camLoc = result.getCameraPose().getTranslation().toTranslation2d();
+    int[] ids = result.getTagIDs();
+    double totalDistance = 0.0;
+
+    for (int id : ids) {
+      totalDistance +=
+          camLoc.getDistance(field.getTagPose(id).get().getTranslation().toTranslation2d());
+    }
+    return totalDistance / ids.length;
+  }
+
+  // Filters
   private boolean camsAgreeWithWheels(Translation3d pose, WallEyeResult result) {
 
-      ChassisSpeeds vel = driveSubsystem.getFieldRelSpeed();
-      Pose2d curPose = driveSubsystem.getPoseMeters();
+    ChassisSpeeds vel = driveSubsystem.getFieldRelSpeed();
+    Pose2d curPose = driveSubsystem.getPoseMeters();
 
-      Translation2d disp = (curPose.getTranslation().minus(pose.toTranslation2d()));
+    Translation2d disp = (curPose.getTranslation().minus(pose.toTranslation2d()));
 
-      double velMagnitude = Math.sqrt(Math.pow(vel.vxMetersPerSecond,2)
-      + Math.pow(vel.vyMetersPerSecond, 2));
+    double velMagnitude =
+        Math.sqrt(Math.pow(vel.vxMetersPerSecond, 2) + Math.pow(vel.vyMetersPerSecond, 2));
 
-      double dispMagnitude = Math.sqrt(Math.pow(disp.getX(),2)+
-      Math.pow(disp.getY(),2));
+    double dispMagnitude = Math.sqrt(Math.pow(disp.getX(), 2) + Math.pow(disp.getY(), 2));
 
-      return result.getNumTags() >= minTags &&
-      dispMagnitude <= (velMagnitude * VisionConstants.kLinearCoeffOnVelFilter 
-      + VisionConstants.kOffsetOnVelFilter
-      + Math.pow(velMagnitude * VisionConstants.kSquaredCoeffOnVelFilter, 2)
-      );
+    return result.getNumTags() >= minTags
+        && dispMagnitude
+            <= (velMagnitude * VisionConstants.kLinearCoeffOnVelFilter
+                + VisionConstants.kOffsetOnVelFilter
+                + Math.pow(velMagnitude * VisionConstants.kSquaredCoeffOnVelFilter, 2));
   }
 
   private boolean camsWithinField(Translation3d pose, WallEyePoseResult result) {
 
-    return (result.getNumTags() >= 2 
-    || result.getAmbiguity() < VisionConstants.kMaxAmbig) 
-    && pose.getMeasureX().in(Meters) < field.getFieldLength() 
-    && pose.getMeasureY().in(Meters) < field.getFieldWidth();
+    return (result.getNumTags() >= 2 || result.getAmbiguity() < VisionConstants.kMaxAmbig)
+        && pose.getMeasureX().in(Meters) < field.getFieldLength()
+        && pose.getMeasureY().in(Meters) < field.getFieldWidth();
   }
-
-  
-
 
   @Override
   public Set<Measure> getMeasures() {
