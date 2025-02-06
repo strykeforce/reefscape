400463e7d6f796d87b756d22247574012979faba
David Shen <dshen1248@gmail.com>
2025-02-06 18:40:41 -0500

    led auto place setter

--- src/main/java/frc/robot/subsystems/led/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/led/LEDSubsystem.java
@@ -1,3 +1,9 @@
 package frc.robot.subsystems.led;
 
-public class LEDSubsystem {}
+public class LEDSubsystem {
+
+  public void setAutoPlacing(boolean isAutoPlacing) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'setAutoPlacing'");
+  }
+}
--- src/main/java/frc/robot/subsystems/robotState/RobotStateSubsystem.java
+++ src/main/java/frc/robot/subsystems/robotState/RobotStateSubsystem.java
@@ -17,11 +17,15 @@ import frc.robot.subsystems.funnel.FunnelSubsystem;
 import frc.robot.subsystems.led.LEDSubsystem;
 import frc.robot.subsystems.tagAlign.TagAlignSubsystem;
 import frc.robot.subsystems.vision.VisionSubsystem;
+
 import java.util.Set;
+
 import org.slf4j.LoggerFactory;
 import org.strykeforce.telemetry.TelemetryService;
 import org.strykeforce.telemetry.measurable.MeasurableSubsystem;
 import org.strykeforce.telemetry.measurable.Measure;
+
+import edu.wpi.first.wpilibj.RobotController;
 
 public class RobotStateSubsystem extends MeasurableSubsystem {
   private org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -163,6 +167,10 @@ public class RobotStateSubsystem extends MeasurableSubsystem {
 
   public void setIsAuto(boolean isAuto) {
     this.isAuto = isAuto;
+  }
+
+  public void setAutoPlacingLed(boolean isAutoPlacing) {
+    ledSubsystem.setAutoPlacing(isAutoPlacing);
   }
 
   private boolean needSafeAlgaeTransfer(RobotStates nextState) {
