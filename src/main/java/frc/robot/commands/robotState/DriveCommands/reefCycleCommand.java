96999e7078e09dae88229601f38288122f14a618
EpicPigGuy <epicpigguysminecraft@gmail.com>
2025-02-10 18:14:42 -0500

    reef cycle update

---
+++ src/main/java/frc/robot/commands/robotState/DriveCommands/reefCycleCommand.java
@@ -0,0 +1,15 @@
+package frc.robot.commands.robotState.DriveCommands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.robotState.RobotStateSubsystem;
+
+public class reefCycleCommand extends InstantCommand {
+    RobotStateSubsystem robotState;
+    public reefCycleCommand(RobotStateSubsystem robotState){
+        this.robotState = robotState;
+    }
+    @Override
+    public void initialize() {
+        robotState.toReefAlign();
+    }
+}
