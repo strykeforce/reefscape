64b03e39174e9f87c53aa2e959aed73470123e36
EpicPigGuy <epicpigguysminecraft@gmail.com>
2025-02-10 18:17:17 -0500

    capitialize

--- src/main/java/frc/robot/commands/robotState/DriveCommands/reefCycleCommand.java
+++ src/main/java/frc/robot/commands/robotState/DriveCommands/reefCycleCommand.java
@@ -3,9 +3,9 @@ package frc.robot.commands.robotState.DriveCommands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.robotState.RobotStateSubsystem;
 
-public class reefCycleCommand extends InstantCommand {
+public class ReefCycleCommand extends InstantCommand {
     RobotStateSubsystem robotState;
-    public reefCycleCommand(RobotStateSubsystem robotState){
+    public ReefCycleCommand(RobotStateSubsystem robotState){
         this.robotState = robotState;
     }
     @Override
