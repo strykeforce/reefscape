844b04843707c26f2e7abf4afd686c4946198969
Isaac Hoekstra <122327870+PotatoBoyH4@users.noreply.github.com>
2025-02-13 20:23:12 -0500

    part 2 of previous

--- src/main/java/frc/robot/commands/robotState/DriveCommands/ReefCycleCommand.java
+++ src/main/java/frc/robot/commands/robotState/DriveCommands/ReefCycleCommand.java
@@ -3,9 +3,9 @@ package frc.robot.commands.robotState.DriveCommands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.robotState.RobotStateSubsystem;
 
-public class ReefCycleComand extends InstantCommand {
+public class ReefCycleCommand extends InstantCommand {
     RobotStateSubsystem robotState;
-    public ReefCycleComand(RobotStateSubsystem robotState){
+    public ReefCycleCommand(RobotStateSubsystem robotState){
         this.robotState = robotState;
     }
     @Override
