68ed8fef7086d1a5eb999a6981c1b9597f5d127b
EpicPigGuy <epicpigguysminecraft@gmail.com>
2025-09-02 18:56:23 -0400

    initalization

---
+++ src/main/java/frc/robot/subsystems/algae/AlgaeIOSim.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.algae;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
+public class AlgaeIOSim implements AlgaeIO{
+    //add code based off mechanical advantage
+}
