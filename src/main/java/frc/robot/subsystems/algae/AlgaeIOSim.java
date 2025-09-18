88d0887c3942db722a03df1139f00de6e5a72fc2
EpicPigGuy <epicpigguysminecraft@gmail.com>
2025-09-18 18:36:36 -0400

    Update AlgaeIOSim.java

--- src/main/java/frc/robot/subsystems/algae/AlgaeIOSim.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeIOSim.java
@@ -1,8 +1,39 @@
 package frc.robot.subsystems.algae;
 
+import frc.robot.constants.AlgaeConstants;
+
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
 public class AlgaeIOSim implements AlgaeIO{
     //add code based off mechanical advantage
+    private final DCMotorSim sim;
+    private final DCMotor gearbox;
+    private double appliedVoltage = 0.0;
+
+    public AlgaeIOSim(DCMotor motorModel, double reduction, double moi) {
+    gearbox = motorModel;
+    sim =
+        new DCMotorSim(LinearSystemId.createDCMotorSystem(motorModel, moi, reduction), motorModel);
+  }
+  @Override
+  public void updateInputs(AlgaeIOInputs inputs) {
+    if (DriverStation.isDisabled()) {
+      runVolts(0.0);
+    }
+
+    sim.update(Constants.loopPeriodSecs);
+    inputs.data =
+        new AlgaeIOData(
+            sim.getAngularPositionRad(),
+            sim.getAngularVelocityRadPerSec(),
+            appliedVoltage,
+            sim.getCurrentDrawAmps(),
+            gearbox.getCurrent(sim.getAngularVelocityRadPerSec(), appliedVoltage),
+            0.0,
+            false,
+            true);
+  }
 }
